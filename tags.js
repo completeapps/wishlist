e63724cd9636767662db7fb3a233bf7d6b307062
complete <bean12810@gmail.com>
2026-05-09 14:21:59 -0500

    Create tags.js

---
+++ tags.js
@@ -0,0 +1,14 @@
+// Edit your tags here!
+// Format: { name: "Tag Name", emoji: "🎁" }
+
+const WISHLIST_TAGS = [
+  { name: "Graduation?", emoji: "🎓" },
+  { name: "General", emoji: "🎁" },
+  { name: "Tech", emoji: "💻" },
+  { name: "Books", emoji: "📚" },
+  { name: "Clothes", emoji: "👕" },
+  { name: "Sports", emoji: "⚽" }
+];
+
+// Don't edit below this line
+window.WISHLIST_TAGS = WISHLIST_TAGS;
